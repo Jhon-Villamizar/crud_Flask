83054b7596109d0205591b7e5b741eb901738989
mesadesoluciones <mesadeservicio@cesde.edu.co>
2019-05-09 20:58:20 -0500

    crud

--- web-store/src/app.py
+++ web-store/src/app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM productos")
+    productos = cur.fetchall()
+    print(productos)
+    return render_template('index.html',data = productos)
 
 @app.route('/add')
 def add():
@@ -23,6 +27,46 @@ def add_product():
         cur.execute("INSERT INTO productos(producto,valor,categoria,descripcion)VALUE(%s,%s,%s,%s)",(producto, valor, categoria, descripcion))
         conn.commit()
         cur.close
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<id>')
+def update(id):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM productos WHERE id={0}".format(id))
+    productos= cur.fetchall()
+
+    return render_template('update_product.html',productos = productos[0])
+
+@app.route('/update_product/<id>', methods=['POST'])
+def update_product(id):
+    # el name en los inputs debe coincidir con el nombre de los campos en la tabla de la base de datos
+    if request.method == 'POST':
+        producto = request.form['producto']
+        valor = request.form['valor']
+        categoria = request.form['categoria']
+        descripcion = request.form['descripcion']
+        cur = conn.cursor()
+        cur.execute("""
+            UPDATE productos
+            SET producto = '{0}',
+                valor = '{1}',
+                categoria = '{2}',
+                descripcion = '{3}'
+            WHERE id = {4}
+        """.format(producto, valor, categoria, descripcion,id))
+
+    return redirect(url_for('index'))
+
+
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM productos WHERE id={0}".format(id))
+    conn.commit()
+    cur.close()
     return redirect(url_for('index'))
     
 if __name__ == '__main__':
